c5b5e7508d67e3d4b53f36acd1bfb92065701882
Hiep <hiepphung2002@gmail.com>
2025-06-22 03:41:51 +0700

    Sua 2

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("✅ MongoDB Connected");
-    } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error.message);
-        process.exit(1); // Dừng app nếu không kết nối được
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
 };
